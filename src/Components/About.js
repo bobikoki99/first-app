1c56f6906d48238b5576de7035614929a53014b7
bobikoki99 <bobikoki99@users.noreply.github.com>
2020-09-18 15:27:40 +0300

    Test 3

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,6 +6,8 @@ function About() {
             <h1>About page</h1>
             <h3>This page is for information</h3>
             <h3>Some Info</h3>
+            <h3>Some Info</h3>
+            <h3>Some Info2</h3>
         </div>
     ) 
 }
